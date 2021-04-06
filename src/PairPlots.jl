635b4b7d6bc3d275b3d08646667fe7aba689dd82
William Thompson <will.thompson@outlook.com>
2021-04-06 16:13:32 -0700

    Take over setting subplot limits to ensure consistency

--- src/PairPlots.jl
+++ src/PairPlots.jl
@@ -43,7 +43,7 @@ function corner(
     # Merge keywords for different subplots & series
     hist_kwargs=merge((; nbins=20, color=:black), hist_kwargs, (;yticks=[]))
     hist2d_kwargs=merge((; nbins=32, color=:Greys, colorbar=:none), hist2d_kwargs)
-    contour_kwargs=merge((; color=:black, linewidth=2), contour_kwargs)
+    contour_kwargs=merge((; color=:black, linewidth=1.5), contour_kwargs)
     scatter_kwargs=merge((; color=:black, alpha=0.1, markersize=1.5), scatter_kwargs)
     percentiles_kwargs=merge((;linestyle=:dash, color=:black), percentiles_kwargs)
     appearance = merge((;
@@ -58,16 +58,30 @@ function corner(
         titlefontsize=10,
     ), appearance)
 
-    threeD = hasproperty(hist2d_kwargs, :seriestype) && hist2d_kwargs.seriestype == :wireframe
+    threeD = get(hist2d_kwargs, :seriestype, nothing) == :wireframe
 
     # Calculate a default reasonable size unless overriden
     s = 250*n
     kwargs = merge((;size = (s,s)), kwargs)
 
+
+    # Rather than messing with linking axes, let's just take over setting the plot limits ourselves
+    ex = [extrema(Tables.getcolumn(table, i)) for i in 1:n]
+    mins = [ex[1] for ex in ex]
+    maxs = [ex[2] for ex in ex]
+    # By how much should we oversize the limits
+    lim_factor = 1.05
+
+
     subplots = []
 
     # Build grid of nxn plots
     for row in 1:n, col in 1:n
+
+        if row < col
+            push!(subplots, RecipesBase.plot(framestyle=:none, background_color_inside=:transparent))
+            continue
+        end
 
         # fmt(label) = "\$\\mathrm{$label}\$"
         fmt(label) = "\$$label\$"
@@ -82,20 +96,25 @@ function corner(
             top_margin = row == 1 ? 10Measures.mm : :match,
             left_margin = col == 1 ? 8Measures.mm : :match,
             right_margin = col == n ? 6Measures.mm : :match,
+
+            xlims=(mins[col], maxs[col]).*lim_factor,
         )
+
+        if row != col
+            kw = (;kw..., ylims=(mins[row], maxs[row]).*lim_factor)
+        end
 
         subplot = 
         if row == col
             # 1D histogram
-            hist(Tables.getcolumn(table, columns[row]), histfunc, merge(appearance, kw, hist_kwargs), plotpercentiles, merge(kw, percentiles_kwargs))
-        elseif row > col
+            hist(Tables.getcolumn(table, row), histfunc, merge(appearance, kw, hist_kwargs), plotpercentiles, merge(kw, percentiles_kwargs))
+        else row > col
             # 2D histogram 
-            hist(Tables.getcolumn(table, columns[row]), view(Tables.getcolumn(table, columns[col]),:), histfunc, merge(appearance, kw, hist2d_kwargs), contour_kwargs, scatter_kwargs, plotcontours, plotscatter)
-        else
-            RecipesBase.plot(framestyle=:none, background_color_inside=:transparent)
+            hist(Tables.getcolumn(table, row), Tables.getcolumn(table, col), histfunc, merge(appearance, kw, hist2d_kwargs), contour_kwargs, scatter_kwargs, plotcontours, plotscatter)
         end
         push!(subplots, subplot)
     end
+
 
     RecipesBase.plot(subplots...; layout=(n,n), legend=:none, kwargs...)
 end
@@ -117,19 +136,16 @@ function hist(a, histfunc, hist_kwargs, plotpercentiles, percentiles_kwargs,)
         pcs = quantile(a_sorted, plotpercentiles./100, sorted=true)
     end
     # WIP: replace the title with an annotation so that the margins don't get messed up.
-    if hasproperty(hist_kwargs, :title) && hist_kwargs.title != ""
+    if get(hist_kwargs, :title, "") != ""
         pcs_title = quantile(a_sorted, [0.16, 0.5, 0.84], sorted=true)
         med = pcs_title[2]
         low = med - pcs_title[1]
         high =  pcs_title[3] - med
         title = @sprintf("\$%s = %.2f^{+%.2f}_{-%.2f}\$", hist_kwargs.title, pcs_title[2], high, low)
         xc = mean(x)
-        yc = maximum(h_scaled) * 1.25
+        yc = maximum(h_scaled) * 1.15
         if hasproperty(hist_kwargs, :titlefontsize)
-            color = :black
-            if hasproperty(hist_kwargs, :titlefontcolor)
-                color=hist_kwargs.titlefontcolor
-            end
+            color = get(hist_kwargs, :titlefontcolor, :black)
             hist_kwargs = merge(
                 delete(hist_kwargs,:title),
                 (;annotation=(xc, yc, (title, color, hist_kwargs.titlefontsize))),
@@ -161,8 +177,8 @@ function hist(a, b, histfunc, hist2d_kwargs, contour_kwargs, scatter_kwargs, plo
     x, y, H = histfunc(a, b, hist2d_kwargs.nbins)
     
     p = RecipesBase.plot()
-    threeD = hasproperty(hist2d_kwargs, :seriestype) && hist2d_kwargs.seriestype == :wireframe
-    if hasproperty(hist2d_kwargs, :seriestype) && hist2d_kwargs.seriestype == :wireframe
+    threeD = get(hist2d_kwargs, :seriestype, nothing) == :wireframe
+    if threeD
         scatter_kwargs = merge(scatter_kwargs, (; seriestype=:scatter3d))
         # if !hasproperty(contour_kwargs, :seriestype)
         #     contour_kwargs = merge(contour_kwargs, (;seriestype=:contour3d))
@@ -170,7 +186,7 @@ function hist(a, b, histfunc, hist2d_kwargs, contour_kwargs, scatter_kwargs, plo
         plotcontours = false
     end
     if plotscatter
-        if hasproperty(scatter_kwargs, :seriestype) && scatter_kwargs.seriestype == :scatter3d
+        if get(scatter_kwargs, :seriestype, nothing) == :scatter3d
             z = zeros(size(y))
             RecipesBase.plot!(p, b, a, z; scatter_kwargs...)
         else
@@ -237,7 +253,8 @@ function __init__()
     @require MCMCChains="c7f686f2-ff18-58e9-bc7b-31028e88f75d" begin
         function corner(chains::MCMCChains.Chains, args...; kwargs...)
             props = keys(chains)
-            values = (reshape(chains[prop].data,:) for prop in props)
+            # values = (reshape(chains[prop].data,:) for prop in props)
+            values = (vec(chains[prop].data) for prop in props)
             table = namedtuple(props, values)
             return corner(table, args...; kwargs...)
         end
