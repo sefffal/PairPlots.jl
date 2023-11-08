cc89b3a5c8d4e203dfb6f6e045f4e7b6eb843bef
William Thompson <wthompson@uvic.ca>
2023-11-07 20:20:39 -0800

    Only include the `parameters` section

--- ext/MCMCChainsExt.jl
+++ ext/MCMCChainsExt.jl
@@ -11,10 +11,15 @@ The columns are named accordingly to the axes of the Matrix (usually :1 though N
 """
 function PairPlots.Series(chains::MCMCChains.Chains; label=nothing, kwargs...)
 
-    column_labels = keys(chains)
+    # To dig out only the `parameters` section without using any
+    # non-public we have to do some convoluted steps
+    sects = sections(chains)
+    i = findfirst(==(:parameters), sects)
+    chains_parameters = MCMCChains.get_sections(chains)[i]
+    column_labels = keys(chains_parameters)
 
     table = NamedTuple([
-        collabel => vec(chains[collabel].data)
+        collabel => vec(chains_parameters[collabel].data)
         for collabel in column_labels
     ])
     PairPlots.Series(label, table, kwargs)
