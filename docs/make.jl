87806868af4a8f955eae6908d5f8a07b7585782e
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-03-28 12:02:53 -0400

    Revert switch to SVG type

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Documenter, DocumenterVitepress, CairoMakie, PairPlots
 
-CairoMakie.activate!(type = "svg")
+CairoMakie.activate!(type = "png", px_per_unit = 3)
 
 ENV["DATAFRAMES_ROWS"] = 5
 
