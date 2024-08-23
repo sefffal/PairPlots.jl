d9a49edda5b6ff37032006f2a5c8186f008e7f87
William Thompson <wthompson@uvic.ca>
2024-08-23 13:08:04 -0700

    Adjust MCMC ext package to work with new options

--- ext/MCMCChainsExt.jl
+++ ext/MCMCChainsExt.jl
@@ -9,7 +9,7 @@ using MCMCChains
 Convenience constructor to build a Series from an abstract matrix.
 The columns are named accordingly to the axes of the Matrix (usually :1 though N).
 """
-function PairPlots.Series(chains::MCMCChains.Chains; label=nothing, kwargs...)
+function PairPlots.Series(chains::MCMCChains.Chains;fullgrid=false,bottomleft=true,topright=fullgrid,bins=Dict{Symbol,Any}(), label=nothing, kwargs...)
 
     chains_parameters = MCMCChains.get_sections(chains, :parameters)
     column_labels = keys(chains_parameters)
@@ -18,7 +18,7 @@ function PairPlots.Series(chains::MCMCChains.Chains; label=nothing, kwargs...)
         collabel => vec(chains_parameters[collabel].data)
         for collabel in column_labels
     ])
-    PairPlots.Series(label, table, kwargs)
+    PairPlots.Series(label, table, bottomleft, topright, bins, kwargs)
 
 end
 end
